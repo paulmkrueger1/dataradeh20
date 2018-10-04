3fb573133582d4b086e929522102beca80899ffd
Jake Berliner <jberliner@electravehicles.com>
2018-10-04 12:44:45 -0400

    - add data_pipeline_helpers - add historical_export script - add Template Workbook to use the helpers (need to update it to use get_user_session)

---
+++ data_pipeline_helpers.py
@@ -0,0 +1,131 @@
+# Imports the Google Cloud client library
+from google.cloud import bigquery
+
+# Instantiates a client
+bigquery_client = bigquery.Client(project='manymoons-215635')
+
+# Import pandas for output format as DataFrame
+import pandas as pd
+import json
+
+from PIL import Image
+import requests
+import numpy as np
+
+def schema_row_to_dict(schema_row):
+	"""
+	schema_row -- <bigquery.table.SchemaRow>
+	"""
+	return {
+        'colname': schema_row.name,
+        'type': schema_row.field_type,
+        'mode': schema_row.mode,
+        'description': schema_row.description,
+        'fields': schema_row.fields
+	}
+
+def describe_table(table='bq_events_all', dataset='dataset_dev'):
+    dataset_ref = bigquery_client.dataset(dataset)
+    bq_events = dataset_ref.table(table)
+    tab = bigquery_client.get_table(bq_events)
+    return pd.DataFrame([schema_row_to_dict(row) for row in tab.schema])
+
+def query_BQ(query):
+    """
+    Main Query Function 
+    
+    Available datasets: 'dataset_dev'
+    
+    Available tables: 'bq_events_all'
+    
+    Example Query: "SELECT DISTINCT(event_type), COUNT(event_type) event_count FROM dataset_dev.bq_events_all GROUP BY event_type"
+    """
+    query_job = bigquery_client.query(query)
+    results = query_job.result()
+    return pd.DataFrame([dict(row) for row in results])
+
+def image_url_to_vector(img_url):
+    """
+    s/o to https://stackoverflow.com/questions/15612373/convert-image-png-to-matrix-and-then-to-1d-array
+    """
+    response = requests.get(img_url, stream=True)
+    response.raw.decode_content = True
+    original_image = Image.open(response.raw)
+    rgba_image = original_image.convert('RGBA')
+    arr = np.array(rgba_image)
+
+    # record the original shape
+    shape = arr.shape
+
+    # make a 1-dimensional view of arr
+    flat_arr = arr.ravel()
+
+    # convert it to a matrix
+    vector = np.matrix(flat_arr)
+
+    # do something to the vector
+    vector[:,::10] = 128
+
+    # reform a numpy array of the original shape
+    arr2 = np.asarray(vector).reshape(shape)
+
+    # make a PIL image
+    img2 = Image.fromarray(arr2, 'RGBA')
+    return arr2
+
+def extract_event_types(out_df, verbose = False):
+    event_type_map = {}
+    failed_event_types = []
+    for event_type in out_df.event_type.unique():
+        event_type_df = out_df[out_df.event_type == event_type]
+        if event_type not in event_type_map:
+            event_type_map[event_type] = []
+        for idx, row in event_type_df.iterrows():
+            try:
+                loaded = json.loads(row["event_properties"].replace("'",'"').replace('n"s',"n's").replace('False', '"False"').replace('True', '"True"'))
+            except Exception as e:
+                if verbose: 
+                    print(e)
+                    print(event_type, row['event_properties'], type(row["event_properties"]))
+                failed_event_types.append(event_type)
+                
+            for key in loaded.keys():
+                if key not in event_type_map[event_type]:
+                    event_type_map[event_type].append(key)
+                    
+    unlanded_types = [e_type for e_type in failed_event_types if e_type not in event_type_map]
+    
+    return {
+        'event_type_map': event_type_map,
+        'failed_event_types': failed_event_types, # event types with at least 1 fail (could still be present in event_type_map)
+        'unlanded_types': unlanded_types # event types that were in the out_df but failed on parsing the keys
+    }
+
+def generate_event_type_map(limit=100000):
+	out_df = query_BQ("""
+	    SELECT event_properties, event_type FROM dataset_dev.bq_events_all ORDER BY RAND() LIMIT {}
+	""".format(limit))
+
+	return extract_event_types(out_df)
+
+def get_user_session(user_id=None, session_number=None, event_types=[], allow_anon=False):
+	query = """
+	    SELECT client_event_time, session_id, event_type, event_properties, user_properties FROM `dataset_dev.bq_events_all`
+	    WHERE session_id = (SELECT session_id FROM dataset_dev.bq_events_all WHERE event_type IN ({})
+	""".format(', '.join(["'{}'".format(i) for i in event_types]))
+
+	if user_id is not None:
+		query += " AND user_id = {} ".format(user_id)
+
+	if session_id is not None:
+		query += " AND session_id = {} ".format(session_number)
+	else:
+		query += " ORDER BY RAND() "
+
+	query += "LIMIT 1) "
+
+	if not allow_anon:
+		query += " AND user_properties NOT LIKE '%anonymousId%' ORDER BY client_event_time"
+
+	out_df = query_BQ(query)
+	return out_df
---
+++ historical_export.py
@@ -0,0 +1,216 @@
+import requests
+import json
+import pandas as pd
+import numpy as np
+# from snowflake.sqlalchemy import URL
+import unidecode
+import zipfile
+import io
+import pyarrow
+import gzip
+import os
+import shutil
+from datetime import datetime, timedelta
+
+# Imports the Google Cloud client library
+from google.cloud import storage
+storage_client = storage.Client(project='manymoons-215635')
+bucket_name = 'raw-events-prod'
+bucket = storage_client.get_bucket(bucket_name)
+
+KEY = "22f09c1b641be78951bc3cc2e21024f9"
+SECRET = "3bfb1703145dab5b1b659f7d05d381d4"
+
+def make_request(endpoint, params = ()):
+    """
+    Generic request function for making API requests to the amplitude API
+    
+    Example Endpoints: https://amplitude.zendesk.com/hc/en-us/articles/205469748-Dashboard-Rest-API-Export-Amplitude-Dashboard-Data
+    """
+    res = requests.get('https://amplitude.com/api/2/'+endpoint, params=params, auth=(KEY, SECRET))
+    if res.status_code == 200:
+        return res.content
+    else:
+        print('Error', res.status_code)
+        return res
+
+def datetime_string(dt):
+    return str(dt).replace(' ','T').replace('-','').split(':')[0]
+
+# Function to check and clear current exported data
+def clear_raw_data(data_path='.'):
+    if '180337' in os.listdir(data_path):
+        shutil.rmtree(os.path.join(data_path,'180337'))
+            
+def prep_columns(df):
+    df.columns = [col.strip('$') for col in list(df.columns)]
+    to_str = ['insert_id', 'schema', 'adid', 'amplitude_event_type', 'city',
+          'client_event_time', 'client_upload_time', 'country', 'data',
+          'device_brand', 'device_carrier', 'device_family', 'device_id',
+          'device_manufacturer', 'device_model', 'device_type', 'dma',
+          'event_properties', 'event_time', 'event_type', 'group_properties',
+          'groups', 'idfa', 'ip_address', 'language', 'library', 'location_lat',
+          'location_lng', 'os_name', 'os_version', 'paying', 'platform', 
+          'processed_time', 'region', 'sample_rate', 'server_received_time',
+          'server_upload_time', 'start_version', 'user_creation_time',
+          'user_id', 'user_properties', 'uuid', 'version_name'
+         ]
+
+    to_list = ['amplitude_attribution_ids']
+
+    to_int = ['amplitude_id', 'app', 'event_id', 'session_id']
+
+    to_bool = ['is_attribution_event']
+    
+    for col in df.columns:
+        if col in to_str:
+	        df[col] = df[col].astype(str)
+        elif col in to_list:
+            df[col] = df[col].astype(list)
+        elif col in to_int:
+            df[col] = df[col].astype(int)
+        elif col in to_bool:
+            df[col] = df[col].astype(bool)
+    
+    return df
+
+def load_zip(zip_fp, raw=False):
+    """
+    Loads compressed zip binary into a pandas DataFrame
+    """
+    failures = []
+    success = 0
+    print(zip_fp)
+    with gzip.GzipFile(zip_fp, 'r') as fin:
+        raw_data = fin.readlines()
+        
+    if raw: return raw_data
+#     str(i)
+    raw_data = [x.strip() for x in raw_data]
+        
+#     raw_data_split = str(raw_data)[2:].split('\\n')
+    
+    parsed_data = []
+    for n, i in enumerate(raw_data):
+        if len(i) > 1:
+            try:
+                parsed_data.append(json.loads(str(i).strip('b\\').strip("'").strip('\\').replace('\\','')))
+                success += 1
+            except Exception as e:
+#                 print(e)
+                failures.append(n)
+            
+#     parsed_data = [json.loads(i.replace('\\','')) for i in raw_data_split if len(i) > 1]
+    print('Success: {}, Failed: {}'.format(success, len(failures)))
+    df = pd.DataFrame(parsed_data)
+    return df, failures
+
+def load_zip_dir(zip_dir, save_individual=False):
+    """
+    Loads all contents of a zip directory into a pandas DataFrame (via load_zip())
+    """
+    dfs = []
+    failures_list = []
+    for n, zip_fp in enumerate(os.listdir(zip_dir)):
+        loaded_percent = round(100.*(n+1)/len(os.listdir(zip_dir)),2)
+        print('Parsing data: {}% complete ...'.format(loaded_percent), end="\r")
+        df, failures = load_zip(os.path.join(zip_dir,zip_fp))
+        if save_individual:
+            df = prep_columns(df)
+            zip_fp = format_gcs_key(zip_fp) + '.parquet'
+#             print('Uploading to: ', zip_fp, '...')
+            
+#             output_name = 
+            save_to_gcs(df, zip_fp)
+        else:
+            dfs.append(df)
+        failures_list.append(failures)
+    
+    if save_individual:
+        return dfs, failures_list
+    else:
+        print('Successfully parsed raw data. Concatenating and returning DF')
+        return pd.concat(dfs), failures_list
+
+def format_gcs_key(zip_fp):
+    return 'data/' + zip_fp[7:-8].replace('-','/').replace('_','/').replace('#','/')
+
+
+def extract_data(start='20180921T07', end='20180921T08', clear_data=True, save_individual=False):
+    """
+    Extracts raw data, decompresses, and loads into pandas DataFrame
+    
+    Kwargs:
+      start -- <str> start date in 'YYYYMMDDTHH' format
+      end -- <str> end date in 'YYYYMMDDTHH' format
+      clear_data -- <bool> whether or not to clear the loaded data before exracting more (Default: True)
+      
+    Return:
+      parsed_data -- <pd.DataFrame> pandas DataFrame of parsed raw data
+    """
+    if clear_data: 
+        print('Clearing workspace ...')
+        clear_raw_data()
+        
+    params = (
+        ('start', start),
+        ('end', end)
+    )
+    print('Exporting data from API ...')
+    data = make_request('export', params)
+    if type(data) == bytes:
+        z = zipfile.ZipFile(io.BytesIO(data))
+        z.extractall() 
+        print('Successfully exported raw data. \nParsing raw data ...')
+        parsed_data, failures = load_zip_dir('180337', save_individual=save_individual)
+        return parsed_data, failures
+    else:
+        print('No data in requested timeframe')
+    
+
+def save_to_gcs(df, dest_key, ext='.parquet'):
+    blob = bucket.blob(dest_key)
+    if ext == '.parquet':
+        df.to_parquet('tmp.parquet')
+    elif ext == '.csv':
+        df.to_csv('tmp.csv')
+    blob.upload_from_filename('tmp'+ext)
+    print('Uploaded to: ', dest_key)
+
+def run_historical_job(start_date=(2017, 9, 17, 12), end_date=(2018, 9, 25, 23), save_individual=True):
+    start_date = datetime(*start_date)
+    end_date = datetime(*end_date)
+    tot_dt = (end_date - start_date).total_seconds() / 3600.
+    all_data = []
+    all_failures = {}
+    for i in range(int(tot_dt)+1):
+        if i % 2 == 0:
+            print(i)
+            start = start_date + timedelta(hours=i)
+            end = start_date + timedelta(hours=i+1)
+            start_str = datetime_string(start)
+            end_str = datetime_string(end)
+            print(start_str, end_str)
+            date_key = str(start).replace('-','/').replace(' ','/').split(':')[0] + '/data.csv'
+            print(date_key)
+            try:
+                loaded_data, failures = extract_data(start_str, end_str, save_individual=save_individual)
+                all_failures[start_date] = failures
+    #             if loaded_data is not None:
+    #                 print(f'Adding {len(loaded_data)} rows', data_key)
+    #                 save_to_gcs(loaded_data, data_key)
+    #                 all_failures[start_date] = failures
+    # #                 all_data.append(loaded_data)
+    #             else:
+    #                 all_failures[start_date] = 'No Data'
+            except Exception as e:
+                print(e)
+                all_failures[start_date] = 'Failed'
+
+            print('-'*20 + '\n')
+#     with open('failures.json', 'w') as fi:
+#         fi.write(json.dump)
+    return all_failures
+    
+if __name__ == '__main__':
+	run_historical_job((2018, 2, 22, 2))
